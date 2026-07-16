d9ebd05c51dce33b883ba8f06d573a0de372c2d8
Asmitha Mohan Raj <asmimvlu@gmail.com>
2026-07-16 14:15:06 +0530

    Update Student.jsx

--- Student.jsx
+++ Student.jsx
@@ -3,7 +3,7 @@ function Student()
   return(
     <div>
       <h2>Student Component:</h2>
-      <p>Name : Afsha </p>
+      <p>Name : Asmitha </p>
       <p>Course : B.Sc.IT</p>
       <p>Semester : V</p>
     </div>
